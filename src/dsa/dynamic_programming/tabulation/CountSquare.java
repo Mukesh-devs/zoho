39cff116397402c21f7bdb813431531adc47f650
mukesh-devs <198108065+Mukesh-devs@users.noreply.github.com>
2025-08-21 00:55:43 +0530

    backup

--- src/dsa/dynamic_programming/tabulation/CountSquare.java
+++ src/dsa/dynamic_programming/tabulation/CountSquare.java
@@ -2,16 +2,16 @@ package dsa.dynamic_programming.tabulation;
 
 import java.util.Arrays;
 
-public class MaximalSquare {
+public class CountSquare {
     public static void main(String[] args) {
-        char[][] matrix = {{'1','1','1','1','0'},{'1','1','1','1','0'},{'1','1','1','1','1'},{'1','1','1','1','1'},{'0','0','1','1','1'}};
-        System.out.println(maximalSquare(matrix));
+        char[][] matrix = {{'0','1','1','1'},{'1','1','1','1'},{'0','1','1','1'}};
+        System.out.println(countSquare(matrix));
     }
-    public static int maximalSquare(char[][] matrix) {
+    public static int countSquare(char[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
         int[][] dp = new int[row][col];
-        int max = 0;
+        int count = 0;
         int temp;
         for ( int i = 0; i < row; i++ ) {
             for ( int j = 0; j < col; j++ ) {
@@ -27,13 +27,13 @@ public class MaximalSquare {
                         dp[i][j] = temp;
                     }
                 }
-                max = Math.max(max,dp[i][j]);
+                count += dp[i][j];
             }
         }
         for ( int i = 0; i < row; i++ ) {
             System.out.println(Arrays.toString(dp[i]));
         }
-        return max * max;
+        return count;
     }
     static int min(int a, int b, int c) {
         if ( a >= b && c >= b) return b;
